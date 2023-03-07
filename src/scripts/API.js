09bc217039ce01f849762b437454191b6f2cce6b
Or Rodrigez <orrodrigez1@gmail.com>
2023-03-07 19:22:01 +0200

    Add display function to show data

--- src/scripts/API.js
+++ src/scripts/API.js
@@ -5,28 +5,61 @@ export default async function getData(location) {
   })
   const data = await response.json();
   try {
-    display(processInfo(data))
+    console.log(data)
+    console.log(processData(data))
+    display(processData(data))
   } catch {
     handleError(data)
   }
 }
 
-function processInfo(data) {
+function processData(data) {
   const name = data.name
-  const temp = data.main.temp
-  const humidity = data.main.humidity
-  const minTemp = data.main.temp_min
-  const maxTemp = data.main.temp_max
-  const visibility = data.visibility
-  const windSpeed = data.wind.speed
-  const clouds = data.clouds.all
-  const rain = data.rain
-  const snow = data.snow
-  return {name, temp, humidity, minTemp, maxTemp, visibility, windSpeed, clouds, rain, snow}
+  const country = data.sys.country
+  const weather = data.weather[0].description
+  const temp = `${data.main.temp}C`
+  const humidity = `${data.main.humidity}%`
+  const minTemp = `${data.main.temp_min}C`
+  const maxTemp = `${data.main.temp_max}C`
+  const windSpeed = `${data.wind.speed}(M/S)`
+  const clouds = `${data.clouds.all}%`
+  let visibility = `${data.visibility}(K/M)`
+  let rain = '-'
+  let snow = '-'
+
+  if(data.visibility === 10000) {
+    visibility = 'good'
+  }
+
+  if(data.snow) snow = data.snow['1h'];
+  if(data.rain) rain = data.rain['1h'];
+  return {name, country, weather, temp, humidity, minTemp, maxTemp, visibility, windSpeed, clouds, rain, snow}
 }
 
-function display(obj) {
+function display(dataObject) {
+  const name = document.getElementById('name')
+  const weather = document.getElementById('weather')
+  const temp = document.getElementById('temp')
+  const humidity = document.getElementById('humidity')
+  const minTemp = document.getElementById('minTemp')
+  const maxTemp = document.getElementById('maxTemp')
+  const visibility = document.getElementById('visibility')
+  const windSpeed = document.getElementById('windSpeed')
+  const clouds = document.getElementById('clouds')
+  const rain = document.getElementById('rain')
+  const snow = document.getElementById('snow')
 
+  name.textContent = `City Name: ${dataObject.name} (${dataObject.country})`
+  weather.textContent = `Weather: ${dataObject.weather}`
+  temp.textContent = `Temperature: ${dataObject.temp}`
+  maxTemp.textContent = `Highest: ${dataObject.maxTemp}`
+  minTemp.textContent = `Lowest: ${dataObject.minTemp}`
+  humidity.textContent = `Humidity: ${dataObject.humidity}`
+  visibility.textContent = `Visibility: ${dataObject.visibility}`
+  windSpeed.textContent = `Wind Speed: ${dataObject.windSpeed}`
+  clouds.textContent = `Clouds: ${dataObject.clouds}`
+  rain.textContent = `Rain: ${dataObject.rain}`
+  snow.textContent = `Snow: ${dataObject.snow}`
 }
 
 function handleError(data) {
