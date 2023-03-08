a742c231283aa582a70e8f84a19156073c1d92a2
Or Rodrigez <orrodrigez1@gmail.com>
2023-03-08 15:47:25 +0200

    Mobile design complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import getData from "./scripts/API";
-
+import './styles/style.css';
+import './styles/main.css';
+import './styles/light-theme.css';
 
 
 let units = 'metric'
 getData('ukraine', units);
 
+const form = document.querySelector('form')
 const givenLocation = document.getElementById('location')
 const searchLocation = document.getElementById('search')
 const changeUnits = document.getElementById('changeUnits')
 const cityName = document.getElementById('name')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  getData(givenLocation.value, units)
+})
 
 searchLocation.addEventListener('click', () => {
   getData(givenLocation.value, units)
--- src/scripts/API.js
+++ src/scripts/API.js
@@ -17,22 +17,22 @@ function processData(data, units) {
   let degree
   let speed
   if(units === 'metric') {
-    degree = 'C'
+    degree = ' °C'
     speed = '(M/S)'
   } else {
-    degree = 'F'
-    speed = '(M/H)'
+    degree = ' °F'
+    speed = '(Mile/H)'
   }
+
   const name = data.name
-  
   const weather = data.weather[0].description
-  const temp = `${data.main.temp}${degree}`
+  const temp = `${data.main.temp.toFixed(1)}${degree}`
   const humidity = `${data.main.humidity}%`
-  const minTemp = `${data.main.temp_min}${degree}`
-  const maxTemp = `${data.main.temp_max}${degree}`
+  const minTemp = `${data.main.temp_min.toFixed(1)}${degree}`
+  const maxTemp = `${data.main.temp_max.toFixed(1)}${degree}`
   const windSpeed = `${data.wind.speed}${speed}`
   const clouds = `${data.clouds.all}%`
-  let visibility = `${data.visibility}(K/M)`
+  let visibility = `${data.visibility}(KM)`
   let country = '-'
   let rain = '-'
   let snow = '-'
@@ -52,38 +52,6 @@ function processData(data, units) {
   return {name, country, localTime, sunrise, sunset, weather, temp, humidity, minTemp, maxTemp, visibility, windSpeed, clouds, rain, snow}
 }
 
-function display(dataObject) {
-  const name = document.getElementById('name')
-  const localTime = document.getElementById('localTime')
-  const sunrise = document.getElementById('sunrise')
-  const sunset = document.getElementById('sunset')
-  const weather = document.getElementById('weather')
-  const temp = document.getElementById('temp')
-  const humidity = document.getElementById('humidity')
-  const minTemp = document.getElementById('minTemp')
-  const maxTemp = document.getElementById('maxTemp')
-  const visibility = document.getElementById('visibility')
-  const windSpeed = document.getElementById('windSpeed')
-  const clouds = document.getElementById('clouds')
-  const rain = document.getElementById('rain')
-  const snow = document.getElementById('snow')
-
-  name.textContent = `City Name: ${dataObject.name} (${dataObject.country})`
-  localTime.textContent = `Local Time: ${dataObject.localTime}`
-  sunrise.textContent = `Sunrise: ${dataObject.sunrise}`
-  sunset.textContent = `Sunset: ${dataObject.sunset}`
-  weather.textContent = `Weather: ${dataObject.weather}`
-  temp.textContent = `Temperature: ${dataObject.temp}`
-  maxTemp.textContent = `Highest: ${dataObject.maxTemp}`
-  minTemp.textContent = `Lowest: ${dataObject.minTemp}`
-  humidity.textContent = `Humidity: ${dataObject.humidity}`
-  visibility.textContent = `Visibility: ${dataObject.visibility}`
-  windSpeed.textContent = `Wind Speed: ${dataObject.windSpeed}`
-  clouds.textContent = `Clouds: ${dataObject.clouds}`
-  rain.textContent = `Rain: ${dataObject.rain}`
-  snow.textContent = `Snow: ${dataObject.snow}`
-}
-
 function convertToLocalTime(timezone, time) {
   let localTime
   if(time) localTime = new Date(time * 1000).getTime() // if given time convert it to local time
@@ -97,6 +65,36 @@ function convertToLocalTime(timezone, time) {
   const hours = `0${cityTime.getHours()}`
   const minutes = `0${cityTime.getMinutes()}`
   return `${hours.slice(-2)}:${minutes.slice(-2)}`
+}
+
+function display(dataObject) {
+  const name = document.getElementById('name')
+  const localTime = document.getElementById('localTime')
+  const temp = document.getElementById('temp')
+  const sunrise = document.getElementById('sunrise')
+  const sunset = document.getElementById('sunset')
+  const weather = document.getElementById('weather')
+  const humidity = document.getElementById('humidity')
+  const minMaxTemp = document.getElementById('minMaxTemp')
+  const visibility = document.getElementById('visibility')
+  const windSpeed = document.getElementById('windSpeed')
+  const clouds = document.getElementById('clouds')
+  const rain = document.getElementById('rain')
+  const snow = document.getElementById('snow')
+
+  name.textContent = `${dataObject.name} (${dataObject.country})`
+  localTime.textContent = `Local Time: ${dataObject.localTime}`
+  temp.textContent = `Currently ${dataObject.temp}`
+  sunrise.textContent = `Sunrise: ${dataObject.sunrise}`
+  sunset.textContent = `Sunset: ${dataObject.sunset}`
+  weather.textContent = dataObject.weather
+  minMaxTemp.textContent = `( Low: ${dataObject.minTemp}, High: ${dataObject.maxTemp} )`
+  humidity.textContent = `Humidity: ${dataObject.humidity}`
+  visibility.textContent = `Visibility: ${dataObject.visibility}`
+  windSpeed.textContent = `Wind: ${dataObject.windSpeed}`
+  clouds.textContent = `Clouds: ${dataObject.clouds}`
+  rain.textContent = `Rain: ${dataObject.rain}`
+  snow.textContent = `Snow: ${dataObject.snow}`
 }
 
 function handleError(data) {
